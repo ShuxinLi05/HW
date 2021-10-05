ed3f21104db561c68ea639dd7b2104eea71f4780
susie <shuxinli0512@outlook.com>
2021-10-05 00:41:06 -0700

    checking sum

--- hw2_sum/src/com/company/Sum.java
+++ hw2_sum/src/com/company/Sum.java
@@ -1,21 +1,23 @@
 package com.company;
 
 public class Sum {
-    public double num;
 
-    public int add(int num1, int num2){
+    public int add(int num1, int num2, int num3){
         int sum = 0;
-        sum = num1 + num2;
+        sum = num1 + num2 + num3;
 
         return sum;
     }
 
-    public double add(double[] nums){
+    public double add(double num4, double num5){
         double sum = 0.0;
+        sum = num4 + num5;
+        /*
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
         }
 
+         */
         return sum;
     }
 
